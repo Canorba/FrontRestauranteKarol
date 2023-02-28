76e4f7ef3f10b1a0a45b31263838ffe778b0f38b
DAVID MONTES <92450281+DAVIDMIRANDAD@users.noreply.github.com>
2023-02-27 20:10:02 -0500

    Implementacion de routing

--- src/app/Components/compras/compras.component.ts
+++ src/app/Components/compras/compras.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-compras',
   templateUrl: './compras.component.html',
   styleUrls: ['./compras.component.css']
 })
-export class ComprasComponent {
+export class ComprasComponent  implements OnInit{
+  constructor(private router: Router){ }
+
+  ngOnInit(): void {}
+
+  navegar(){
+    this.router.navigate(['/persona']);
+  }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComprasComponent } from './Components/compras/compras.component';
+import { EgresosComponent } from './Components/egresos/egresos.component';
+import { FacturaComponent } from './Components/factura/factura.component';
+import { InventarioComponent } from './Components/inventario/inventario.component';
+import { InventarioPlatilloComponent } from './Components/inventario-platillo/inventario-platillo.component';
+import { PedidoComponent } from './Components/pedido/pedido.component';
+import { PersonaComponent } from './Components/persona/persona.component';
+import { PlatilloComponent } from './Components/platillo/platillo.component';
+import { PlatilloXFacturaComponent } from './Components/platillo-xfactura/platillo-xfactura.component';
+import { ProductoComponent } from './Components/producto/producto.component';
+import { ProveedorComponent } from './Components/proveedor/proveedor.component';
+import { UsuarioComponent } from './Components/usuario/usuario.component';
+import { HomeComponent } from './Components/home/home.component';
+import { FormularioComponent } from './Components/formulario/formulario.component';
 
-const routes: Routes = [];
+
+
+// Rutas de navegacion
+const routes: Routes = [
+{path:'', redirectTo: '/Home', pathMatch: 'full' } ,
+{path:'Home',component: HomeComponent } ,
+{path:'Compras',component: ComprasComponent} ,
+{path:'Egresos',component: EgresosComponent},
+{path:'Factura',component: FacturaComponent},
+{path:'Inventario',component: InventarioComponent},
+{path:'InventarioPlatillo',component: InventarioPlatilloComponent},
+{path:'Pedido',component: PedidoComponent},
+{path:'Persona',component: PersonaComponent},
+{path:'Platillo',component: PlatilloComponent},
+{path:'PlatilloPorFactura',component: PlatilloXFacturaComponent},
+{path:'Producto',component: ProductoComponent},
+{path:'Proveedor',component: ProveedorComponent},
+{path:'Usuarios',component: UsuarioComponent},
+{path:'Formulario',component: FormularioComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
