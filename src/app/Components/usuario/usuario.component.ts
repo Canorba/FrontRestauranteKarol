f7c5f9d972bce1b05e49e058f9093e63b57510c4
DAVID MONTES <92450281+DAVIDMIRANDAD@users.noreply.github.com>
2023-04-13 21:20:38 -0500

    Implementacion del metodo get y creacion de tabla con datos

--- src/app/Components/usuario/usuario.component.ts
+++ src/app/Components/usuario/usuario.component.ts
@@ -11,17 +11,11 @@ export class UsuarioComponent {
   constructor(public api: ApiService) { }
 
   ngOnInit(): void {
-    this.GetUsuario();
-    this.PostUsuario();
+  
   }
   
 
-  public GetUsuario() {
-    this.api.get("Usuario")
-  }
-  public PostUsuario() {
-    this.api.post("Usuario", )
-  }
+ 
 
 
 }
