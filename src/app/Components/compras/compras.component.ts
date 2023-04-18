31f559017d194326418c0bc42ed4becb8060ac92
CARLOS ANTONIO ORDOÑEZ BAQUERO <baquerok62@gmail.com>
2023-04-18 00:38:34 -0500

    Creacion del metodo get en compras, egresos, factura e inventario

--- src/app/Components/compras/compras.component.ts
+++ src/app/Components/compras/compras.component.ts
@@ -1,18 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApiService } from 'src/app/Services/api.service';
 
 @Component({
   selector: 'app-compras',
   templateUrl: './compras.component.html',
   styleUrls: ['./compras.component.css']
 })
-export class ComprasComponent  implements OnInit{
-  constructor(private router: Router){ }
-
-  ngOnInit(): void {}
-
-  navegar(){
-    this.router.navigate(['/persona']);
+export class ComprasComponent  implements OnInit {
+  column:Object;
+  displayedColumns: string[]=[]
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(public Api: ApiService){
+    this.dataSource=new MatTableDataSource();
   }
 
+  ngOnInit(): void {
+      this.Getcompra();
+  }
+
+  public async Getcompra(){
+     await this.Api.get("Compras").then((res)=>{
+     
+      this.displayedColumns=Object.keys(res[0])
+        // this.loadTable([res[0]])
+        this.dataSource.data=res
+    });
+    this.dataSource.paginator=this.paginator;
+    this.dataSource.sort=this.sort
+  }
+
+  // loadTable(data:any[]){
+  //   for(let column in data[0]){
+  //     this.displayedColumns.push(column)
+  //   }
+  // }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
--- src/app/Components/factura/factura.component.ts
+++ src/app/Components/factura/factura.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApiService } from 'src/app/Services/api.service';
 
 @Component({
   selector: 'app-factura',
   templateUrl:'./factura.component.html',
   styleUrls: ['./factura.component.css']
 })
-export class FacturaComponent {
+export class FacturaComponent implements OnInit {
+  column:Object;
+  displayedColumns: string[]=[]
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(public Api: ApiService){
+    this.dataSource=new MatTableDataSource();
+  }
 
+  ngOnInit(): void {
+      this.Getegresos();
+  }
+
+  public async Getegresos(){
+     await this.Api.get("Facturas").then((res)=>{
+     
+      this.displayedColumns=Object.keys(res[0])
+        // this.loadTable([res[0]])
+        this.dataSource.data=res
+    });
+    this.dataSource.paginator=this.paginator;
+    this.dataSource.sort=this.sort
+  }
+
+  // loadTable(data:any[]){
+  //   for(let column in data[0]){
+  //     this.displayedColumns.push(column)
+  //   }
+  // }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
+
+
+
