a2e825a4669a5101e92ac4c1bc3e9b7e73128e22
DAVID MONTES <92450281+DAVIDMIRANDAD@users.noreply.github.com>
2023-04-17 20:04:43 -0500

    Implementacion del metodo Get

--- src/app/Components/producto/producto.component.ts
+++ src/app/Components/producto/producto.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApiService } from 'src/app/Services/api.service';
 
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
   styleUrls: ['./producto.component.css']
 })
-export class ProductoComponent {
+export class ProductoComponent implements OnInit {
+  column:Object;
+  displayedColumns: string[]=[]
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(public Api: ApiService){
+    this.dataSource=new MatTableDataSource();
+  }
 
+  ngOnInit(): void {
+      this.Getproducto();
+  }
+
+  public async Getproducto(){
+     await this.Api.get("Productoes").then((res)=>{
+     
+      this.displayedColumns=Object.keys(res[0])
+        // this.loadTable([res[0]])
+        this.dataSource.data=res
+    });
+    this.dataSource.paginator=this.paginator;
+    this.dataSource.sort=this.sort
+  }
+
+  // loadTable(data:any[]){
+  //   for(let column in data[0]){
+  //     this.displayedColumns.push(column)
+  //   }
+  // }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
--- src/app/Components/proveedor/proveedor.component.ts
+++ src/app/Components/proveedor/proveedor.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApiService } from 'src/app/Services/api.service';
 
 @Component({
   selector: 'app-proveedor',
   templateUrl: './proveedor.component.html',
   styleUrls: ['./proveedor.component.css']
 })
-export class ProveedorComponent {
+export class ProveedorComponent implements OnInit {
+  column:Object;
+  displayedColumns: string[]=[]
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(public Api: ApiService){
+    this.dataSource=new MatTableDataSource();
+  }
+  ngOnInit(): void {
+    this.Getproveedor();
+}
+public async Getproveedor(){
+  await this.Api.get("Proveedors").then((res)=>{
+  
+   this.displayedColumns=Object.keys(res[0])
+     // this.loadTable([res[0]])
+     this.dataSource.data=res
+ });
+ this.dataSource.paginator=this.paginator;
+ this.dataSource.sort=this.sort
+}
+applyFilter(event: Event) {
+  const filterValue = (event.target as HTMLInputElement).value;
+  this.dataSource.filter = filterValue.trim().toLowerCase();
 
+  if (this.dataSource.paginator) {
+    this.dataSource.paginator.firstPage();
+  }
+}
 }
