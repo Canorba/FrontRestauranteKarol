989a8eb2d3e003cca5083914416878bde55637c2
DAVID MONTES <92450281+DAVIDMIRANDAD@users.noreply.github.com>
2023-04-20 20:10:08 -0500

    Implementacion del metodo get en los componentes restantes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,13 @@ import { ComprasComponent } from './Components/compras/compras.component';
 import { EgresosComponent } from './Components/egresos/egresos.component';
 import { FacturaComponent } from './Components/factura/factura.component';
 import { InventarioComponent } from './Components/inventario/inventario.component';
-import { InventarioPlatilloComponent } from './Components/inventario-platillo/inventario-platillo.component';
 import { PedidoComponent } from './Components/pedido/pedido.component';
 import { PersonaComponent } from './Components/persona/persona.component';
 import { PlatilloComponent } from './Components/platillo/platillo.component';
-import { PlatilloXFacturaComponent } from './Components/platillo-xfactura/platillo-xfactura.component';
 import { ProductoComponent } from './Components/producto/producto.component';
 import { ProveedorComponent } from './Components/proveedor/proveedor.component';
 import { UsuarioComponent } from './Components/usuario/usuario.component';
 import { HomeComponent } from './Components/home/home.component';
-import { FormularioComponent } from './Components/formulario/formulario.component';
 
 // Rutas de navegacion
 const routes: Routes = [
@@ -23,15 +20,13 @@ const routes: Routes = [
 {path:'Egresos',component: EgresosComponent},
 {path:'Factura',component: FacturaComponent},
 {path:'Inventario',component: InventarioComponent},
-{path:'InventarioPlatillo',component: InventarioPlatilloComponent},
 {path:'Pedido',component: PedidoComponent},
 {path:'Persona',component: PersonaComponent},
 {path:'Platillo',component: PlatilloComponent},
-{path:'platillo-xfactura',component: PlatilloXFacturaComponent},
 {path:'Producto',component: ProductoComponent},
 {path:'Proveedor',component: ProveedorComponent},
 {path:'Usuario',component: UsuarioComponent},
-{path:'Formulario',component: FormularioComponent}
+
 
 ];
 
